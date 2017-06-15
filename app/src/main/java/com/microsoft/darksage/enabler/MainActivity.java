77cd1dc7ef019f6606c4a9471bf9125e5ddfe815
Shantanu Mishra <shantanu.m28@gmail.com>
2017-06-15 19:59:34 +0530

    Ripple Effect on Click on a view

--- app/src/main/java/com/microsoft/darksage/enabler/MainActivity.java
+++ app/src/main/java/com/microsoft/darksage/enabler/MainActivity.java
@@ -1,13 +1,23 @@
 package com.microsoft.darksage.enabler;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.View;
+import android.view.MenuItem;
+import android.view.Menu;
+
+
+import android.widget.*;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.ripple_effect_in_android_imageview_images);
     }
 }
